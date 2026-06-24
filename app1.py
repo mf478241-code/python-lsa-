9265eeb3343a0cbee1d110bab9abea58bfda9a86
Mohammed Faiz <mf478241@gmail.com>
2026-06-24 18:12:42 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,109 @@
+# a="20"
+# b=10
+# c=40
+# d=15
+# e="34"
+# f="28"
+# print(int(a)+int(e)+int(f))
+# print(b-c+d)
+# print(int(a)+int(b)+int(f))
+# g=79
+# print("my marks in maths is",g) 
+
+
+
+# a=10
+# b=8.6
+# c="my is faiz"
+# d=True
+# print(type(a),a)
+# print(type(b),b)
+# print(type(c),c)
+# print(type(d),d)
+
+# age=23
+# print(type(age),age)
+
+# name="faiz"
+# print(name)
+# print(len(name))
+
+
+
+# / division 
+
+
+
+
+
+# a=17
+# b=5
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b # division 
+# g=a//b # floor division, integer division 
+# h=a%b # modulo division 
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h) 
+
+
+# x=10
+# y=20
+# x,y=y,x
+# print(x,y)
+
+
+
+# age=20
+# c=age+5
+# print(c)
+
+
+# price=99.99
+# gst=99.99%18
+# print(gst)
+
+# price = 99.99
+# gst = price * 18 / 100
+# final_price = price + gst
+
+# print(final_price)
+
+
+# name=input("my name is:")
+# phonenumber=input("my phn is:")
+# college=input("college name:")
+# print(name)
+# print(phonenumber)
+# print(college)
+
+# name=str(input("what is your name:"))
+# age=int(input("what is your age:"))
+# birth=2026-age
+# print("hello", name, "we both are of same birth year that is" , birth)
+
+# name=str(input("what is your name:"))
+# age=int(input("what is your age:"))
+# maths_marks=float(input("what is your marks:"))
+# print(f"Name: {name} , Age:{age}")
+# print(f"Marks: {maths_marks:.1f}%") 
+
+# total = int(input("Enter seconds: "))
+# hours = total // 3600
+# minutes = (total % 3600) // 60
+# seconds = total % 60
+# print(f"Hours: {hours} , minutes:{minutes} , seconds: {seconds}")
+
+
+# width=float(input("width:"))
+# height=float(input("height:"))
+# area=width*height
+# perimeter=2*(width+height)
+# print(f"Area: {area}, Perimeter: {perimeter}")
+
+
