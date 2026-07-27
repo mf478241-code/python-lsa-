eab0b312c5196d8641c438450de522f7c5b70e37
Mohammed Faiz <mf478241@gmail.com>
2026-07-27 14:20:13 +0530

    Delete firebase_config.py

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase only once
-if not firebase_admin._apps:
-    cred = credentials.Certificate("credentials/firebase_key.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
